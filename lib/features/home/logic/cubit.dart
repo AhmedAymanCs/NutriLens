b7a44d9cc6df810331fca7ae71632a3a037dedcb
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-05-01 10:13:54 +0300

    home states and cubit done

--- lib/features/home/logic/cubit.dart
+++ lib/features/home/logic/cubit.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nutrilens/features/home/data/model/home_data_model.dart';
 import 'package:nutrilens/features/home/data/repository/repositroy.dart';
 part 'state.dart';
 
@@ -8,4 +9,21 @@ class HomeCubit extends Cubit<HomeState> {
   final HomeRepository _homeRepository;
 
   HomeCubit(this._homeRepository) : super(const HomeState());
+
+  Future<void> getHomeData() async {
+    emit(state.copyWith(status: HomeStatus.loading));
+    final result = await _homeRepository.getHomeData();
+    result.fold(
+      (failure) => emit(state.copyWith(
+        status: HomeStatus.failure,
+        errorMessage: failure,
+      )),
+      (homeDataModel) => emit(
+        state.copyWith(
+          status: HomeStatus.success,
+          homeDataModel: homeDataModel,
+        ),
+      ),
+    );
+  }
 }
--- lib/features/home/logic/state.dart
+++ lib/features/home/logic/state.dart
@@ -4,12 +4,26 @@ enum HomeStatus { initial, loading, success, failure }
 
 class HomeState extends Equatable {
   final HomeStatus status;
+  final HomeDataModel? homeDataModel;
+  final String? errorMessage;
 
-  const HomeState({this.status = HomeStatus.initial});
-  HomeState copyWith({HomeStatus? status}) {
-    return HomeState(status: status ?? this.status);
+  const HomeState({
+    this.status = HomeStatus.initial,
+    this.homeDataModel,
+    this.errorMessage,
+  });
+  HomeState copyWith({
+    HomeStatus? status,
+    HomeDataModel? homeDataModel,
+    String? errorMessage,
+  }) {
+    return HomeState(
+      status: status ?? this.status,
+      homeDataModel: homeDataModel ?? this.homeDataModel,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
   }
 
   @override
-  List<Object?> get props => [status];
+  List<Object?> get props => [status, homeDataModel, errorMessage];
 }
