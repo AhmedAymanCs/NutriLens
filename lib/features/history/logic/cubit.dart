da317b8c82b596d9c72ac0a0b1357b5d288e9a90
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-05-01 18:21:38 +0300

    history states and cubit done

--- lib/features/history/logic/cubit.dart
+++ lib/features/history/logic/cubit.dart
@@ -1,9 +1,19 @@
-import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:nutrilens/features/History/data/repository/History_repository.dart';
-part 'state.dart';
+import 'package:nutrilens/features/history/data/repository/history_repository.dart';
+import 'package:nutrilens/features/history/logic/state.dart';
 
 class HistoryCubit extends Cubit<HistoryState> {
   final HistoryRepository _historyRepository;
-  HistoryCubit(this._historyRepository) : super(const HistoryState());
+  HistoryCubit(this._historyRepository) : super(HistoryState(selectedDate: DateTime.now()));
+
+  Future<void> fetchHistory(DateTime date) async {
+    emit(state.copyWith(status: HistoryStatus.loading, selectedDate: date));
+    
+    final result = await _historyRepository.getHistoryByDate(date);
+    
+    result.fold(
+      (error) => emit(state.copyWith(status: HistoryStatus.failure, error: error)),
+      (data) => emit(state.copyWith(status: HistoryStatus.success, historyData: data)),
+    );
+  }
 }
--- lib/features/history/logic/state.dart
+++ lib/features/history/logic/state.dart
@@ -1,18 +1,39 @@
-part of 'cubit.dart';
+
+
+
+import 'package:equatable/equatable.dart';
+import 'package:nutrilens/features/history/data/model/history_data_model.dart';
 
 enum HistoryStatus { initial, loading, success, failure }
 
 class HistoryState extends Equatable {
+  
   final HistoryStatus status;
-  final String error;
-  const HistoryState({this.status = HistoryStatus.initial, this.error = ''});
-  HistoryState copyWith({HistoryStatus? status, String? error}) {
+  final String? error;
+  final HistoryDataModel? historyData;
+  final DateTime selectedDate;
+  
+  const HistoryState({
+    this.status = HistoryStatus.initial,
+    this.error,
+    this.historyData,
+    required this.selectedDate,
+  });
+
+  HistoryState copyWith({
+    HistoryStatus? status, 
+    String? error, 
+    HistoryDataModel? historyData,
+    DateTime? selectedDate,
+  }) {
     return HistoryState(
       status: status ?? this.status,
       error: error ?? this.error,
+      selectedDate: selectedDate ?? this.selectedDate,
+      historyData: historyData ?? this.historyData,
     );
   }
 
   @override
-  List<Object?> get props => [status, error];
+  List<Object?> get props => [status, error, historyData, selectedDate];
 }
