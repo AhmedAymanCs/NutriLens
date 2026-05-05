bbcd1af444cf8cf30b28db48d63135bdd217032b
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-05-06 01:40:50 +0300

    finish edition of history feature

---
+++ lib/core/models/user_model.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserModel {
+  final String uid, email;
+  final String name, photoURL;
+  final String gender;
+  final String goal;
+  final int age;
+  final double height;
+  final double weight;
+
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.name,
+    required this.photoURL,
+    this.gender = "",
+    this.goal = "",
+    this.age = 0,
+    this.height = 0.0,
+    this.weight = 0.0,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'uid': uid,
+      'name': name,
+      'email': email,
+      'photo_url': photoURL,
+      'gender': gender,
+      'goal': goal,
+      'age': age,
+      'height': height,
+      'weight': weight,
+    };
+  }
+
+  factory UserModel.fromFirebaseAuth(User user, String name) {
+    return UserModel(
+      uid: user.uid,
+      email: user.email!,
+      name: user.displayName ?? name,
+      photoURL: user.photoURL ??
+          "https://cutiedp.com/wp-content/uploads/2025/08/no-dp-image-5.webp",
+    );
+  }
+
+  factory UserModel.fromFirestore(Map<String, dynamic> json) {
+    return UserModel(
+      uid: json['uid'],
+      name: json['name'] ?? "Unknown Name",
+      email: json['email'],
+      photoURL:
+          json['photo_url'] ??
+          "https://cutiedp.com/wp-content/uploads/2025/08/no-dp-image-5.webp",
+      gender: json['gender'] ?? "",
+      goal: json['goal'] ?? "",
+      age: json['age'] ?? 0,
+      height: json['height'] ?? 0.0,
+      weight: json['weight'] ?? 0.0,
+    );
+  }
+}
