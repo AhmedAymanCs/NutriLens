ecf21c9a99e579601dbcfedcfbfe2fb4b59aedb6
Ahmed <benzemaa46@gmail.com>
2026-05-23 17:34:05 +0300

    refactor: Remove unused SVG import from splash screen

--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:nutrilens/core/constants/color_manager.dart';
 import 'package:nutrilens/core/constants/font_manager.dart';
 import 'package:nutrilens/core/constants/image_manager.dart';
